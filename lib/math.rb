b79d48575d50f5e7eeffaa0cd4b25311636d3e57
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 18:12:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,30 +1,32 @@
 def addition(num1,num2)
+ 
  sum=9 
+
   puts sum
 end
   
 addition(4,5)
 
-
-
-
-
-
 def subtraction(num1, num2)
 
  difference=5
+
  puts difference
 
 end
+
 subtraction(10,5)
 
 def division(num1, num2)
-division(50/2)
+
 puts qotient
+
 qotient=25
 
 
 end
+
+division(50/2)
 
 def multiplication(num1, num2)
 
