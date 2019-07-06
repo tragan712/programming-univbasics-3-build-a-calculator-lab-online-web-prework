bb58e8af0a0e9591d6b2c2d9593a2f563da8839a
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 18:30:07 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
-def addition(num1, num2)
-
+def addition(num1,num2)
+ sum=9 
+p sum
 end
-
+  addition(4,5)
 def subtraction(num1, num2)
-
+difference=5
+p difference
 end
-
+subtraction(10,5)
 def division(num1, num2)
-
+qotient=25
+p qotient
 end
-
+division(50,2)
 def multiplication(num1, num2)
-
+product=120
+p product
 end
-
+multiplication(4,30)
 def modulo(num1, num2)
-
+remainder=4
+p remainder
 end
-
+modulo(34,5)
 def square_root(num)
-
+square_root=9
+p square_root
 end
+square_root(81)
+
+
+
