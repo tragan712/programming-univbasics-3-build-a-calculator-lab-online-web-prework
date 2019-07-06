1a373371ea48499437ed0ef79f58a2320098849a
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 17:07:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
 
+addition= 5,4
+
 end
 
 def subtraction(num1, num2)
+subtraction= 10,5
+
 
 end
 
 def division(num1, num2)
+division= 5,2
+
 
 end
 
 def multiplication(num1, num2)
+multiplication= 5,5
+
 
 end
 
 def modulo(num1, num2)
+modulo=(9,2)
+
+
 
 end
 
 def square_root(num)
+square_root(9)
+Math.sqrt
 
 end
